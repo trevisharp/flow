38f43cf9585b4eaf06f7c759f0f91e7fb8063d1a
Trevisan <38115923+trevisharp@users.noreply.github.com>
2023-01-31 13:03:34 -0300

    Improve Teste example

--- Test/Program.cs
+++ Test/Program.cs
@@ -3,13 +3,16 @@ using System.IO;
 
 using FlowPattern;
 
-DirectoryFlow.Create("/")
+"/".OpenDirectoryFlow()
     .If(x => x is FileInfo)
         .Act(x => WriteLine($"Arquivo: {x.Name}"))
     .Ret
     .If(x => x is DirectoryInfo)
         .If(x => x.Name.Length < 8)
             .Act(x => WriteLine($"Pasta com nome pequeno: {x.Name}"))
+        .Ret
+        .If(x => x.Name.Length > 8)
+            .Act(x => WriteLine($"Pasta com nome grande: {x.Name}"))
         .Ret
     .Ret
 .Start();
